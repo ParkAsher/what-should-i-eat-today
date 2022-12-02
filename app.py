e828f9ab244dd47c4b4817524cbc67349823b534
ParkAsher <devasherpark@naver.com>
2022-12-02 19:25:13 +0900

    feat : header component styling & login page mapping

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+# login.html mapping
+@app.route('/login')
+def login_page():
+    return render_template('index.html', component_name='login')
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
---
+++ static/js/header.js
@@ -0,0 +1,23 @@
+const links = document.querySelectorAll('#header .header-wrap .link a');
+
+links.forEach(
+    (item) => {
+        item.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const href = this.getAttribute('href');
+
+            // 추가 작업 요망!
+            switch (href) {
+                case "./login.html":
+                    $.ajax({
+                        type: "GET",
+                        url: "/login",
+                        success: function (response) {
+                            console.log(response)
+                        }
+                    })
+            }
+        })
+    }
+)
