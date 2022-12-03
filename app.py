c35ad6a7a1950955589cd8d351432d6c8b79922d
ParkAsher <devasherpark@naver.com>
2022-12-03 23:53:58 +0900

    my config save

--- app.py
+++ app.py
@@ -23,6 +23,27 @@ def write_page():
     return render_template('index.html', component_name='write')
 
 
+# register api
+@app.route('/api/user-register', methods=['POST'])
+def user_register():
+    userId = request.form['id']
+    userPw = request.form['pw']
+    userName = request.form['name']
+    userEmail = request.form['email']
+
+    sql = "INSERT INTO user(user_id, user_pw, user_name, user_email) VALUES (%s, %s, %s, %s)"
+
+    app.database.execute(sql, (userId, userPw, userName, userEmail)).lastrowid
+
+    return jsonify({'msg' : "등록성공!"})
+
+
 
 if __name__ == '__main__':
+
+    app.config.from_pyfile("config.py")
+
+    database = create_engine(app.config['DB_URL'], encoding='utf-8', max_overflow=0)
+    app.database = database
+
     app.run('0.0.0.0', port=5000, debug=True)
