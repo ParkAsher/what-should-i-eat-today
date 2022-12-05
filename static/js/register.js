8448f027ecf32416e3b7bbb648589958680884c8
boktato <fimjsl47@gmail.com>
2022-12-05 15:49:22 +0900

    feat : exception handling ver.2

--- static/js/register.js
+++ static/js/register.js
@@ -8,9 +8,23 @@ function register() {
     let userName = $('#user-name').val();
     let userEmail = $('#user-email').val();
 
+    let nickname = userNickname.search(/[ㄱ-ㅎ|ㅏ-ㅣ]/g);
+    
+    let idKorean = userId.search(/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/g);
+    let idSpace = userId.search(/[`~!@@#$%^&*|₩₩₩'₩";:₩/?]/gi);
+
     let pwNumber = userPw.search(/[0-9]/g);
-    let pwEnglish = userPw.search(/[a-z]/ig);
+    let pwEnglish = userPw.search(/[a-z]/gi);
     let pwSpece = userPw.search(/[`~!@@#$%^&*|₩₩₩'₩";:₩/?]/gi);
+
+    let nameNumber = userName.search(/[0-9]/g);
+    let nameSpace = userName.search(/[`~!@@#$%^&*|₩₩₩'₩";:₩/?]/gi);
+
+    // 이메일 기본 정규식
+    // let emailCheck = userEmail.search(/^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i);
+
+    // RFC 5322 이메일 형식 강화형
+    let emailCheck = userEmail.search(/^[-0-9A-Za-z!#$%&'*+/=?^_`{|}~.]+@[-0-9A-Za-z!#$%&'*+/=?^_`{|}~]+[.]{1}[0-9A-Za-z]/i);
 
     console.log($('#user-nickname').attr('valid'))
     // 예외 처리 조건문
@@ -18,8 +32,19 @@ function register() {
         alert("닉네임을 입력해주세요.")
         return;
     }
+    if (userNickname.length < 2 || userNickname.length > 12) {
+        alert("닉네임은 2자리 ~ 12자리 이내로 입력해주세요.");
+        return;
+    }
+    if (nickname > -1){
+        alert("올바르지 못한 닉네임 형식입니다.")
+    }
     if (userId === "") {
         alert("아이디를 입력해주세요.")
+        return;
+    }
+    if (idSpace > -1 || idKorean > -1) {
+        alert("아이디는 한글 또는 특수 기호(*)가 포함될 수 없습니다.");
         return;
     }
     if (userId.length < 5 || userId.length > 20) {
@@ -42,12 +67,21 @@ function register() {
         alert("이름을 입력해주세요.")
         return;
     }
+    if (nameNumber > -1 || nameSpace > -1) {
+        alert("이름은 한글 또는 영어만 입력 가능합니다.");
+        return;
+    }
     if ($('#user-nickname').attr('valid') == "default" || $('#user-id').attr('valid') == "default") {
         alert("닉네임과 아이디의 중복체크를 진행해주세요.")
         return;
     }
     if ($('#user-nickname').attr('valid') == "false" || $('#user-id').attr('valid') == "false") {
         alert("사용가능한 닉네임과 아이디를 입력해주세요.")
+        return;
+    }
+    if (emailCheck !== 0){
+        alert("알맞는 이메일 형식이 아닙니다.")
+        console.log(emailCheck)
         return;
     }
     if (userPw !== userPwCheck) {
