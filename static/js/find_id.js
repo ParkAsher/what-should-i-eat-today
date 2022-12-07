167b8ce579102c14e7995144bc9a32dbd96f6273
Lee Jiyoung <dasom1910@naver.com>
2022-12-07 15:49:27 +0900

    feat: find id api

--- static/js/find_id.js
+++ static/js/find_id.js
@@ -13,33 +13,18 @@ function find_id() {
         url: "/api/find-user-id",
         data: { name: userName, email: userEmail },
         success: function (response) {
-
-            if (response['result'] === "Name-Not-Found") {
-                // 1. 이름이 없거나
-                $('#find-err-msg-name').empty()
-                $('#find-err-msg-email').empty()
-
-                $('#find-err-msg-name').css('color', 'red')
-                $('#find-err-msg-name').append("존재하지 않는 회원 이름입니다.")
-                return;
+            if (response['success'] === false) {
+                $('#find-msg').empty()
+                $('#find-msg').css('color', 'red')
+                $('#find-msg').css('text-align', 'center')
+                $('#find-msg').append("존재하지 않는 회원입니다.")
+            } else {
+                $('#find-msg').empty()
+                $('#find-msg').css('color', 'blue')
+                $('#find-msg').css('text-align', 'center')
+                $('#find-msg').append(response['user_id_find'][0]["user_id"])
             }
-
-            if (response['result'] === "Email-Not-Correct") {
-                // 2. 이름은 있는데 이메일이 틀렸거나
-                $('#find-err-msg-name').empty()
-                $('#find-err-msg-email').empty()
-
-                $('#find-err-msg-email').css('color', 'red')
-                $('#find-err-msg-email').append("이메일이 일치하지 않습니다.")
-                return;
-            }
-
-            if (response['result'] === "Find-Success") {
-                // 아이디 찾기 성공
-                $('#find-scs-msg').css('color', 'blue')
-                // append 메시지에 아이디 정보 전달??
-                 $('#find-scs-msg').append("")
-            }
+    
         }
     })
 }
